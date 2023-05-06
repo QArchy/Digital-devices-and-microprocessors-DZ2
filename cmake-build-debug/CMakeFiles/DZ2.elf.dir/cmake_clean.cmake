53c853ed6b155c63a2d47213a256c335f841ad66
Худяков Артём Сергеевич <xyarce@mail.ru>
2023-05-06 20:15:58 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/DZ2.elf.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DZ2.elf.dir/src/Startup/startup_stm32f051r8tx.s.obj"
+  "CMakeFiles/DZ2.elf.dir/src/main.c.obj"
+  "CMakeFiles/DZ2.elf.dir/src/main.c.obj.d"
+  "DZ2.elf"
+  "DZ2.elf.manifest"
+  "DZ2.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/DZ2.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
